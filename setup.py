56e39f428d289837c6d8ec5881fdaa14f58b6d0b
David Hughes <d@vidhughes.com>
2012-08-12 07:08:43 -0700

    Bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import setuptools
 setuptools.setup(
     name='basebox',
-    version='0.1.0',
+    version='0.1.1',
     packages = setuptools.find_packages(),
     install_requires = ['fabric', 'cuisine', 'proxytypes'],
 )
