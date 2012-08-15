0886f5d7c7595bb4777eeb78ba74529808c898cf
David Hughes <d@vidhughes.com>
2012-08-15 11:20:28 -0700

    Added stub for CLI

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ import setuptools
 setuptools.setup(
     name='basebox',
     version='0.1.1',
-    packages = setuptools.find_packages(),
-    install_requires = ['fabric', 'cuisine', 'proxytypes'],
+    packages=setuptools.find_packages(),
+    install_requires=['fabric', 'cuisine', 'proxytypes'],
+    entry_points={
+        'console_scripts': [
+            'basebox = basebox.cli:main'
+            ]
+        }
 )
-
