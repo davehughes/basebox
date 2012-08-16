820f471088966f72ca48b998c3579dab64c5b96f
David Hughes <d@vidhughes.com>
2012-08-16 14:23:54 -0700

    Updated setup.py for package registration

--- setup.py
+++ setup.py
@@ -3,8 +3,13 @@ import setuptools
 setuptools.setup(
     name='basebox',
     version='0.1.2',
+    url='https://github.com/davehughes/basebox',
+    author='David Hughes',
+    author_email='d@vidhughes.com',
+    description='A small library for interacting with Vagrant boxes using Fabric',
+
     packages=setuptools.find_packages(),
-    install_requires=['fabric', 'cuisine', 'proxytypes'],
+    install_requires=['fabric', 'cuisine>=0.3.2', 'proxytypes'],
     entry_points={
         'console_scripts': [
             'basebox = basebox.cli:main'
